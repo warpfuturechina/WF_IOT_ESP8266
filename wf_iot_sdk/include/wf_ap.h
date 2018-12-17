4bb60c9942ac6c75af67ec8f64cedcebe41bdb3a
信勇哥得水牛 <517661395@qq.com>
2018-12-17 18:52:23 +0800

    sdk update

---
+++ wf_iot_sdk/include/wf_ap.h
@@ -0,0 +1,37 @@
+#ifndef __WF_AP_H__
+#define __WF_AP_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <esp_err.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  char *ssid;
+  char *password;
+  uint8_t random_num;
+} wf_ap_result_t;
+
+typedef enum {
+  ERR_MANUAL_STOP = 0,
+} wf_ap_error_t;
+
+typedef struct {
+    void (*complete)(wf_ap_result_t*);
+    void (*error)(wf_ap_error_t);
+} wf_ap_callback_t;
+
+esp_err_t wf_ap_start(const char *ap_ssid_prefix, const char *ap_password, wf_ap_callback_t callback);
+
+void wf_ap_stop();
+
+const char *wf_ap_version();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ wf_iot_sdk/include/wf_http.h
@@ -0,0 +1,36 @@
+#ifndef __WF_HTTP_H__
+#define __WF_HTTP_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <time.h>
+#include <esp_err.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define WF_HTTP_NO_ERR 0
+#define WF_HTTP_NO_MEM_ERR -1
+#define WF_HTTP_URL_ERR -2
+#define WF_HTTP_LOAD_CERT_ERR -3
+#define WF_HTTP_DNS_ERR -4
+#define WF_HTTP_CONNECT_ERR -5
+#define WF_HTTP_SOCKET_ERR -6
+#define WF_HTTP_TLS_ERR -7
+
+typedef void* wf_http_handle_t;
+
+int wf_http_get(const char *url, void (*on_data)(void *, const uint8_t *, size_t), void *param, wf_http_handle_t *handler);
+
+int wf_http_start(wf_http_handle_t handler);
+
+ssize_t wf_http_yield(wf_http_handle_t handler);
+
+void wf_http_clean(wf_http_handle_t handler);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
